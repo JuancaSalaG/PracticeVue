8035a240bd33dc7d983e5e6d62d71e1b1301c8f9
nouscode <juancspace@outlook.com>
2024-12-08 13:31:11 -0500

    termina layout clase 5

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,27 @@
         <template #header>
             <Header />
         </template>
+        <template #resume>
+            <Resume />
+        </template>
+        <template #movements>
+            <Movements />
+        </template>
     </Layout>
 </template>
 
 <script>
-import Layout from './Layout.vue'; "@/components/Layout.vue";
+import Layout from "@/components/Layout.vue";
 import Header from "@/components/Header.vue";
+import Resume from '@/components/Resume/Index.vue';
+import Movements from '@/components/Movements/Index.vue';
 
 export default {
     components: {
         Layout,
-        Header
+        Header,
+        Resume,
+        Movements
     }
 }
 </script>
